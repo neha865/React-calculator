7d36f419f2364b2dae8bb2bf5ba7ff9fe8abc9ff
neha865 <ch.en.u4cse21147@ch.students.amrita.edu>
2022-08-10 15:31:33 +0530

    deploy my react app to github pages

--- src/App.js
+++ src/App.js
@@ -1,24 +1,52 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [result, setResult] = useState('');
+
+  const handleClick = (event) => {
+    setResult(result.concat(event.target.value));
+  };
+
+  const clearDisplay = () => {
+    setResult('');
+  };
+
+  const calculate = () => {
+    setResult(eval(result).toString());
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <body>
+        <div className='cal' >
+          <input type='text' className='input' placeholder="0" value={result} />
+          <input type='button' value='9' className='buttons' onClick={handleClick} />
+          <input type='button' value='8' className='buttons' onClick={handleClick} />
+          <input type='button' value='7' className='buttons' onClick={handleClick} />
+          <input type='button' value='6' className='buttons' onClick={handleClick} />
+          <input type='button' value='5' className='buttons' onClick={handleClick} />
+          <input type='button' value='4' className='buttons' onClick={handleClick} />
+          <input type='button' value='3' className='buttons' onClick={handleClick} />
+          <input type='button' value='2' className='buttons' onClick={handleClick} />
+          <input type='button' value='1' className='buttons' onClick={handleClick} />
+          <input type='button' value='0' className='buttons' onClick={handleClick} />
+
+          <input type='button' value='+' className='buttons' onClick={handleClick} />
+          <input type='button' value='-' className='buttons' onClick={handleClick} />
+          <input type='button' value='/' className='buttons' onClick={handleClick} />
+          <input type='button' value='*' className='buttons' onClick={handleClick} />
+          <input type='button' value='%' className='buttons' onClick={handleClick} />
+          <input type='button' value='.' className='buttons' onClick={handleClick} />
+
+          <input type='button' value='Clear' className='but' onClick={clearDisplay} />
+          <input type='button' value='=' className='but' onClick={calculate} />
+
+
+        </div>
+      </body>
+      <p className='des'>- Built by Sree Neha using Reactjs. </p>
+    </>
   );
 }
 
